97b1fc6e531ee533db463848a5d15fb777b83864
Chairuabda <chairuabda@gmail.com>
2023-10-02 10:21:35 +0700

    Create Exercise-7-app

---
+++ exercise-7-app/src/App.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home"
+import Counter from "./components/counter"
+import StopWatch from './components/stopwatch'
+import Filter from './components/filter'
+import ChatBox from './components/chatBox'
+
+function App() {
+
+  return (
+    <div className="App">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/counter" element={<Counter />} />
+				<Route path="/stopwatch" element={<StopWatch />} />
+				<Route path="/filter" element={<Filter />} />
+				<Route path="/chatbox" element={<ChatBox />} />
+			</Routes>
+		</div>
+  )
+}
+
+export default App
