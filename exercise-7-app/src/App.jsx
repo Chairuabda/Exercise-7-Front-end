bca76377d7bb1b4e734aeedd28536c5528901d52
bashasibuan <46988897+bashasibuan@users.noreply.github.com>
2023-10-02 15:46:14 +0700

    update stopwatch

--- exercise-7-app/src/App.jsx
+++ exercise-7-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/home/index";
 // import Counter from "./components/counter";
-// import StopWatch from "./components/stopwatch";
+import StopWatch from "./components/stopwatch";
 // import Filter from "./components/filter";
 // import ChatBox from "./components/chatBox";
 
@@ -10,9 +10,9 @@ function App() {
 		<div className="App">
 			<Routes>
 				<Route path="/" element={<Home />} />
-				{/* <Route path="/counter" element={<Counter />} />
+				{/* <Route path="/counter" element={<Counter />} /> */}
 				<Route path="/stopwatch" element={<StopWatch />} />
-				<Route path="/filter" element={<Filter />} /> */}
+				{/* <Route path="/filter" element={<Filter />} /> */}
 				{/* <Route path="/chatbox" element={<ChatBox />} /> */}
 			</Routes>
 		</div>
---
+++ exercise-7-app/src/components/stopwatch/index.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import { Flex, Stack, VStack, HStack, Box, Button, ButtonGroup } from "@chakra-ui/react";
+
+const Stopwatch = () => {
+    const [time,setTime] = useState(0);
+    const [running, setRunning] = useState(false);
+    useEffect(() => {
+        let interval;
+        if (running) {
+            interval = setInterval(() =>{
+                setTime((prevTime) => prevTime + 10);
+            }, 10);
+        }else if (!running) {
+            clearInterval(interval);
+        }
+        return () => clearInterval(interval);
+    }, [running]);
+
+    return (
+            <VStack className="stopwatch" backgroundColor="black" width="100%" margin="auto" paddingTop="50px" fontWeight="lg" height="100vh">
+                <Box className="numbers" color="white" fontSize="4rem">
+                <span>{("0" + Math.floor((time / 60000)) % 60).slice(-2)}:</span>
+                <span>{("0" + Math.floor((time / 1000) %60 )).slice(-2)}:</span>
+                <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+                </Box>
+                <ButtonGroup className="buttons">
+                <Button onClick={() => setRunning(true)}backgroundColor="green" color="white" padding="25px">Start</Button>
+                <Button onClick={() => setRunning(false)}backgroundColor="red" color="white" padding="25px">Stop</Button>
+                <Button onClick={() => setTime(0)}backgroundColor="yellow" color="white" padding="25px">Reset</Button>
+                </ButtonGroup>
+            </VStack>
+    )
+}
+export default Stopwatch;
